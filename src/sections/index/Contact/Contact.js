f05052adce9a074f2522064c72d6c4aa0718d92e
kmw111 <kmw111@ha.org.hk>
2021-03-10 17:26:57 +0800

    [Feature] Use preload instead of prefetch

--- src/sections/index/Contact/Contact.js
+++ src/sections/index/Contact/Contact.js
@@ -14,10 +14,10 @@ import useSx from "./useContactSx";
 const contactFormFallback = <ContactFormFallback />;
 const personalInfoFallback = <PersonalInfoFallback />;
 
-const ContactForm = loadable(() => import(/* webpackPrefetch: true */ "./ContactForm"), {
+const ContactForm = loadable(() => import(/* webpackPreload: true */ "./ContactForm"), {
   fallback: contactFormFallback
 });
-const PersonalInfo = loadable(() => import(/* webpackPrefetch: true */ "./PersonalInfo"), {
+const PersonalInfo = loadable(() => import(/* webpackPreload: true */ "./PersonalInfo"), {
   fallback: personalInfoFallback
 });
 
