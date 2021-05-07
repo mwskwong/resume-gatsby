0b033be12bb3bd9109577806c6fcf723c583ed16
kmw111 <kmw111@ha.org.hk>
2021-05-07 14:07:54 +0800

    [Optimization] Use preact

--- gatsby-config.js
+++ gatsby-config.js
@@ -78,6 +78,7 @@ module.exports = {
       }
     },
     "gatsby-plugin-webpack-bundle-analyser-v2",
-    "gatsby-plugin-robots-txt"
+    "gatsby-plugin-robots-txt",
+    "gatsby-plugin-preact"
   ]
 };
