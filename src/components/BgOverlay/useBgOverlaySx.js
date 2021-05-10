d4ab67455549aaf19b93006abe0bd27b97bfedba
kmw111 <kmw111@ha.org.hk>
2021-05-10 14:02:24 +0800

    [Bug Fix] Flash of non darken BG

--- src/components/BgOverlay/useBgOverlaySx.js
+++ src/components/BgOverlay/useBgOverlaySx.js
@@ -3,7 +3,7 @@ import { useMemo } from "react";
 const useSx = ({ sxProp }) => useMemo(() => ({
   root: {
     position: "relative",
-    zIndex: 1,
+    zIndex: 9,
     bgcolor: "rgba(0, 0, 0, .7)",
     width: "100%",
     height: "100%",
