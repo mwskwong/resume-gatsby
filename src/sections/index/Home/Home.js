2b0b2dfcf7a3d60999513465f1e8e6891b1fe976
kmw111 <kmw111@ha.org.hk>
2021-05-11 08:47:12 +0800

    [Debug] Check if the correct branch is accessed

--- src/sections/index/Home/Home.js
+++ src/sections/index/Home/Home.js
@@ -49,6 +49,7 @@ const Home = () => {
         <Toolbar />
         <ThemeProvider mode="dark">
           <Container sx={sx.container}>
+            This is the next branch
             <Title />
             <SocialMedia sx={sx.socialMedia} />
             <DownloadResumeButton sx={sx.downloadResumeButton} />
