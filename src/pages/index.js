95be4077a993dc5ea0229add71983da7d304b69a
kmw111 <kmw111@ha.org.hk>
2021-05-27 10:03:15 +0800

    [Test] Lazy Hydration

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,28 +11,45 @@ import {
 
 import About from "sections/index/About";
 import { Box } from "@material-ui/core";
+import LazyHydrate from "react-lazy-hydration";
 import NavBar from "components/NavBar";
 import SEO from "components/SEO";
 import ScrollToTopFab from "components/ScrollToTopFab";
 
 const Index = () => {
-  // const sectionHydrate = { rootMargin: "0px" };
-
   return (
     <Box component="main">
       <SEO />
       <NavBar />
       <Home />
-      <About />
-      <FunFact />
-      <Education />
-      <Work />
-      <Project />
-      <Testimonial />
-      <Contact />
-      <Footer />
-      <ScrollToTopFab />
-    </Box>
+      <LazyHydrate whenVisible whenIdle>
+        <About />
+      </LazyHydrate>
+      <LazyHydrate whenVisible>
+        <FunFact />
+      </LazyHydrate>
+      <LazyHydrate whenVisible whenIdle>
+        <Education />
+      </LazyHydrate>
+      <LazyHydrate whenVisible whenIdle>
+        <Work />
+      </LazyHydrate>
+      <LazyHydrate whenVisible whenIdle>
+        <Project />
+      </LazyHydrate>
+      <LazyHydrate whenVisible whenIdle>
+        <Testimonial />
+      </LazyHydrate>
+      <LazyHydrate whenVisible whenIdle>
+        <Contact />
+      </LazyHydrate>
+      <LazyHydrate whenVisible whenIdle>
+        <Footer />
+      </LazyHydrate>
+      <LazyHydrate whenVisible whenIdle>
+        <ScrollToTopFab />
+      </LazyHydrate>
+    </Box >
   );
 };
 
