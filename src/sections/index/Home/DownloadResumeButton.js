1a4ed1034f59c37778e249d18bb773808f8a4621
kmw111 <kmw111@ha.org.hk>
2021-05-31 09:54:49 +0800

    [QoL] Use GraphQL to query resume

--- src/sections/index/Home/DownloadResumeButton.js
+++ src/sections/index/Home/DownloadResumeButton.js
@@ -1,20 +1,31 @@
+import { graphql, useStaticQuery } from "gatsby";
+
 import { Button } from "@material-ui/core";
 import PropTypes from "prop-types";
 import constants from "contents/constants";
-import resume from "documents/resume.pdf";
 
-const DownloadResumeButton = ({ sx }) => (
-  <Button
-    sx={sx}
-    variant="outlined"
-    size="large"
-    href={resume}
-    target="_blank"
-    rel="noopener noreferrer"
-  >
-    {constants.downloadResumeButtonText}
-  </Button>
-);
+const DownloadResumeButton = ({ sx }) => {
+  const { file: resume } = useStaticQuery(graphql`
+    query {
+      file(relativePath: {eq: "resume.pdf"}) {
+        publicURL
+      }
+    }
+  `);
+
+  return (
+    <Button
+      sx={sx}
+      variant="outlined"
+      size="large"
+      href={resume.publicURL}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      {constants.downloadResumeButtonText}
+    </Button>
+  );
+};
 
 DownloadResumeButton.propTypes = {
   sx: PropTypes.object
