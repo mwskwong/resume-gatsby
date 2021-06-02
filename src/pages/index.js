3f380f499499a6593ef4d519003f73ff2e9c1f6f
kmw111 <kmw111@ha.org.hk>
2021-06-02 10:01:31 +0800

    [Optimization] Add lazy hydrate

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,24 +11,43 @@ import {
 } from "sections/index";
 
 import { Box } from "@material-ui/core";
+import LazyHydrate from "react-lazy-hydration";
 import NavBar from "components/NavBar";
 import SEO from "components/SEO";
 import ScrollToTopFab from "components/ScrollToTopFab";
 
 const Index = () => {
+  const lazyHydrateOptions = { rootMargin: "0px" };
+
   return (
     <Box component="main">
       <SEO />
       <NavBar />
       <Home />
-      <About />
-      <FunFact />
-      <Education />
-      <Work />
-      <Project />
-      <Testimonial />
-      <Contact />
-      <Footer />
+      <LazyHydrate whenVisible={lazyHydrateOptions}>
+        <About />
+      </LazyHydrate>
+      <LazyHydrate whenVisible={lazyHydrateOptions}>
+        <FunFact />
+      </LazyHydrate>
+      <LazyHydrate whenVisible={lazyHydrateOptions}>
+        <Education />
+      </LazyHydrate>
+      <LazyHydrate whenVisible={lazyHydrateOptions}>
+        <Work />
+      </LazyHydrate>
+      <LazyHydrate whenVisible={lazyHydrateOptions}>
+        <Project />
+      </LazyHydrate>
+      <LazyHydrate whenVisible={lazyHydrateOptions}>
+        <Testimonial />
+      </LazyHydrate>
+      <LazyHydrate whenVisible={lazyHydrateOptions}>
+        <Contact />
+      </LazyHydrate>
+      <LazyHydrate whenVisible={lazyHydrateOptions}>
+        <Footer />
+      </LazyHydrate>
       <ScrollToTopFab />
     </Box>
   );
