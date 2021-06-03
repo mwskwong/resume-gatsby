d719506125fbef2e980da19e2022667ccb123374
kmw111 <kmw111@ha.org.hk>
2021-06-03 08:49:58 +0800

    [Feature] Auto reload PWA

--- src/actions/actions.js
+++
@@ -1,6 +0,0 @@
-import { SW_UPDATE_READY } from "./actionTypes";
-
-export const swUpdateReady = () => ({
-  type: SW_UPDATE_READY,
-  payload: {}
-});
--- src/reducers/index.js
+++
@@ -1,4 +0,0 @@
-import { combineReducers } from "redux";
-import swUpdateReady from "./swUpdateReady";
-
-export default combineReducers({ swUpdateReady });
