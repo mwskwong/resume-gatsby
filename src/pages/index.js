ffd603717d08d52b05c40bfaa1174173ca16332b
kmw111 <kmw111@ha.org.hk>
2021-06-21 14:55:56 +0800

    Revert "[Test] Suspense"
    This reverts commit 89cd03b32fc302c7b1537e226fdc4a22e615dead.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,17 @@
+// import {
+//   About,
+//   Contact,
+//   Education,
+//   Footer,
+//   FunFact,
+//   Home,
+//   Project,
+//   Testimonial,
+//   Work
+// } from "sections/index";
 import { Suspense, lazy } from "react";
 
 import { Box } from "@material-ui/core";
-import Home from "sections/index/Home";
 import NavBar from "components/NavBar";
 import SEO from "components/SEO";
 import ScrollToTopFab from "components/ScrollToTopFab";
@@ -11,6 +21,7 @@ const Contact = lazy(() => import("sections/index/Contact"));
 const Education = lazy(() => import("sections/index/Education"));
 const Footer = lazy(() => import("sections/index/Footer"));
 const FunFact = lazy(() => import("sections/index/FunFact"));
+const Home = lazy(() => import("sections/index/Home"));
 const Project = lazy(() => import("sections/index/Project"));
 const Testimonial = lazy(() => import("sections/index/Testimonial"));
 const Work = lazy(() => import("sections/index/Work"));
@@ -22,32 +33,7 @@ const Index = () => {
     <Box component="main">
       <SEO />
       <NavBar />
-      <Home />
       <Suspense fallback={null}>
-        <About />
-      </Suspense>
-      <Suspense fallback={null}>
-        <FunFact />
-      </Suspense>
-      <Suspense fallback={null}>
-        <Education />
-      </Suspense>
-      <Suspense fallback={null}>
-        <Work />
-      </Suspense>
-      <Suspense fallback={null}>
-        <Project />
-      </Suspense>
-      <Suspense fallback={null}>
-        <Testimonial />
-      </Suspense>
-      <Suspense fallback={null}>
-        <Contact />
-      </Suspense>
-      <Suspense fallback={null}>
-        <Footer />
-      </Suspense>
-      {/* <Suspense fallback={null}>
         <Home />
         <About />
         <FunFact />
@@ -57,9 +43,9 @@ const Index = () => {
         <Testimonial />
         <Contact />
         <Footer />
-      </Suspense> */}
+      </Suspense>
       <ScrollToTopFab />
-    </Box >
+    </Box>
   );
 };
 
